cad6f56d83563dfed8c28229e7799d03f6da9d4e
Chris Briggs <coldfire_vortex@hotmail.com>
2015-11-06 09:55:40 +1000

    Added AddEntityFramework to ConfigureServices

--- ng-MusicStore-API/src/ng-MusicStore-API/Startup.cs
+++ ng-MusicStore-API/src/ng-MusicStore-API/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.Data.Entity;
 using Microsoft.Dnx.Runtime;
 using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
+using ng_MusicStore_Models;
 
 namespace ng_MusicStore_API
 {
@@ -38,6 +40,11 @@ namespace ng_MusicStore_API
             // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
             // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
             // services.AddWebApiConventions();
+
+            services.AddEntityFramework()
+    .AddSqlServer()
+    .AddDbContext<MusicStoreContext>(options =>
+        options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
         }
 
         // Configure is called after ConfigureServices is called.
