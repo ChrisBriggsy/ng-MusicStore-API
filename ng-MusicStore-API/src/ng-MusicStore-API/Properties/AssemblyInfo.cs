dfa44eaf8304ffa0c5fdc64e1884361685d33978
Chris Briggs <coldfire_vortex@hotmail.com>
2015-11-06 09:12:59 +1000

    Initial Commit

---
+++ ng-MusicStore-API/src/ng-MusicStore-API/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ng_MusicStore_API")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ng_MusicStore_API")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("78e9558e-13d7-4754-b85a-76fd5b5f8f03")]
